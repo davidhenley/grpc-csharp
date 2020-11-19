ba08554108a679db5cf4e1a5a9351bddf144caca
David Henley <henleydr@gmail.com>
2020-11-18 19:58:03 -0600

    gRPC client

--- Client/Client.cs
+++ Client/Client.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using Grpc.Core;
+using Shared;
 
 namespace Client
 {
-    class Client
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var host = args[0];
+            var port = int.Parse(args[1]);
+            var x = long.Parse(args[2]);
+            var op = args[3];
+            var y = long.Parse(args[4]);
+
+            var channel = new Channel(host, port, ChannelCredentials.Insecure);
+            
+            var client = new Svc.SvcClient(channel);
+            var reply = client.Calculate(new CalculateRequest
+            {
+                X = x,
+                Op = op,
+                Y = y
+            });
+            
+            Console.WriteLine($"The calculated result is: {reply.Result}");
         }
     }
 }
